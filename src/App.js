c18529ce2c4b438bbe0632d08bd90ba2dc45f176
Rommel Sanchez Coronel <rommel.alan.sanchez@gmail.com>
2022-10-11 15:06:39 -0500

    Created components and expenses folders

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import "./App.css";
+import Expenses from "./components/Expenses/Expenses";
+
+const App = () => {
+  const expenses = [
+    { title: "Car Insurance", amount: 237.39, date: new Date(2021, 2, 22) },
+    { title: "Supplies", amount: 3337.39, date: new Date(2021, 2, 23) },
+    { title: "Meal", amount: 27.39, date: new Date(2021, 2, 24) },
+    { title: "Shoes", amount: 223.39, date: new Date(2021, 2, 25) },
+  ];
+
+  return (
+    <div className="expenses">
+      <Expenses expenses={expenses}></Expenses>
+    </div>
+  );
+};
+
+export default App;
